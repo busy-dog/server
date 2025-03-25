9453ab3097a4b3e804b1f35990253539fe091781
mango <setsunamango@gmail.com>
2025-03-25 21:22:32 +0800

    feat: 新增用户文件上传API

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -10,29 +10,30 @@ export const register = (app: Hono) => {
   /**
    * 用户训练数据同步
    */
-  app.post('/member/record/async',
+  app.post(
+    '/member/record/async',
     validator('cookie', async (_, ctx) => {
       const res = await session.get(ctx);
       const { access_token: token } = res ?? {};
       if (isString(token)) return { token };
       return ctx.json(decorator(new Error('Github token not find')), 401);
-    })
-    ,
+    }),
     async ({ req, json }) => {
-    try {
-      const { AWS_S3_BUCKET: bucket } = process.env;
-      if (!isString(bucket)) {
-        throw new Error('AWS_S3_BUCKET is not set');
+      try {
+        const { AWS_S3_BUCKET: bucket } = process.env;
+        if (!isString(bucket)) {
+          throw new Error('AWS_S3_BUCKET is not set');
+        }
+
+        await aws.s3.put(Buffer.from(await req.arrayBuffer()), {
+          bucket,
+        });
+
+        return json(decorator(null));
+      } catch (error) {
+        report.error(error);
+        return json(decorator(error));
       }
-
-      await aws.s3.put(Buffer.from(await req.arrayBuffer()), {
-        bucket,
-      });
-
-      return json(decorator(null));
-    } catch (error) {
-      report.error(error);
-      return json(decorator(error));
-    }
-  });
+    },
+  );
 };
